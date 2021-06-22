fe783eb2682b4f057524cdc7a51fbe738326fa11
Anmol Toppo <anmoltoppo@live.in>
2021-06-22 19:38:23 +0530

    updated document

--- Azure/modules/iam_module/Modules/role_definition/variables.tf
+++ Azure/modules/iam_module/Modules/role_definition/variables.tf
@@ -8,8 +8,8 @@ variable "enviroment" {
     type = map
     default = {
 
-        "infrastructure_ID" = "string"
-        "system_ID" = "string"
+        "infra" = "string"
+        "env" = "string"
     }
     validation = {
         condition     = var.Enviroment == "Dev" || var.Enviroment == "QA" || var.Enviroment == "Test" || var.Enviroment == "Prod"
